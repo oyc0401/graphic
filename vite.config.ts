98ca1c6a3338bf5a63e198ab8f1a57da94c6af29
oyuchan <oyc0401@gmail.com>
2025-04-25 02:35:02 +0900

    휠 트랙패드 개선

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,7 @@ export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait(), svgr()],
   server: {
     host: "0.0.0.0",
-    allowedHosts: [
-      "474901b9-b1fa-4d22-8f03-6e9abf61ed38-00-3f3714blflx8h.sisko.replit.dev",
-    ],
+
     hmr: false,
   },
   build: {
