68782dc37ba70f075ddd27cf4a25f7e1dbcab04b
yuchan <oyc0401@gmail.com>
2025-09-15 15:52:01 +0900

    디버그 모드 해제

--- src/app/position.ts
+++ src/app/position.ts
@@ -140,12 +140,12 @@ export function setDefaultPosition() {
   let x = (position.screenWidth - width) / 2;
   let y = (position.screenHeight - height) / 2;
 
-  // //디버깅용!
-  width = 20;
-  height = 20;
-  scale = 60;
-  x = 10;
-  y = 10;
+  // // //디버깅용!
+  // width = 20;
+  // height = 20;
+  // scale = 60;
+  // x = 10;
+  // y = 10;
 
   position.setScale(scale);
   position.setWidth(Math.floor(width));
