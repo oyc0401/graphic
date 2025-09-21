a6e911961901e6b2346c2a27da869fa26d095069
yuchan <oyc0401@gmail.com>
2025-09-21 19:08:30 +0900

    로딩중 전체 막기

--- src/app/paintState.ts
+++ src/app/paintState.ts
@@ -5,80 +5,85 @@ type Action = "BRUSH" | "ZOOM" | "PINCH" | "PAN"; // 키보드 떼면 brush로 �
 type ToolId = "brush" | "select" | "selection" | "resize"; // 선택창 풀면 brush로 됌
 type BrushId = "brush" | "eraser" | "liquify";
 class PaintState {
-    action: Action = "BRUSH";
-    toolId: ToolId = "brush";
-    brushId: BrushId = "brush";
-    brushSize = { brush: 5, eraser: 10, liquify: 50 };
-    brushAlpha = { brush: 100, eraser: 100, liquify: 100 };
+  action: Action = "BRUSH";
+  toolId: ToolId = "brush";
+  brushId: BrushId = "brush";
+  brushSize = { brush: 5, eraser: 10, liquify: 50 };
+  brushAlpha = { brush: 100, eraser: 100, liquify: 100 };
 
-    cursorX = 0;
-    cursorY = 0;
+  cursorX = 0;
+  cursorY = 0;
 
-    pointerdown = false;
-    drawing = false;
-    canTouch = true;
+  pointerdown = false;
+  drawing = false;
+  canTouch = true;
 
-    targetId = "brush";
+  targetId = "brush";
 
-    changed = false;
-    showCircle = false;
-    showSizeHandle = false;
+  changed = false;
+  showCircle = false;
+  showSizeHandle = false;
 
-    constructor() {
-        makeAutoObservable(this);
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    setAction(val: Action) {
-        this.action = val;
+  setAction(val: Action) {
+    this.action = val;
+  }
+  setToolId(toolId: ToolId) {
+    this.toolId = toolId;
+  }
+  setBrushId(brushId: BrushId) {
+    this.brushId = brushId;
+    this.targetId = brushId;
+  }
+  setPointerdown(val: boolean) {
+    this.pointerdown = val;
+  }
+  setDrawing(val: boolean) {
+    this.drawing = val;
+  }
+  setCanTouch(val: boolean) {
+    if (val) {
+      //   setTimeout(() => {
+      this.canTouch = val;
+      //   }, 3000);
+    } else {
+      this.canTouch = val;
     }
-    setToolId(toolId: ToolId) {
-        this.toolId = toolId;
-    }
-    setBrushId(brushId: BrushId) {
-        this.brushId = brushId;
-        this.targetId = brushId;
-    }
-    setPointerdown(val: boolean) {
-        this.pointerdown = val;
-    }
-    setDrawing(val: boolean) {
-        this.drawing = val;
-    }
-    setCanTouch(val: boolean) {
-        this.canTouch = val;
-    }
-    setCursorPosition(x, y) {
-        this.cursorX = x;
-        this.cursorY = y;
-    }
-    setBrushSize(size: number) {
-        this.brushSize[this.targetId] = size;
-        const worker = getLayerWorker();
-        worker.setStrokeSize(size);
-    }
-    setBrushAlpha(alpha: number) {
-        this.brushAlpha[this.targetId] = alpha;
-    }
-    setShowCircle(value) {
-        this.showCircle = value;
-    }
+  }
+  setCursorPosition(x, y) {
+    this.cursorX = x;
+    this.cursorY = y;
+  }
+  setBrushSize(size: number) {
+    this.brushSize[this.targetId] = size;
+    const worker = getLayerWorker();
+    worker.setStrokeSize(size);
+  }
+  setBrushAlpha(alpha: number) {
+    this.brushAlpha[this.targetId] = alpha;
+  }
+  setShowCircle(value) {
+    this.showCircle = value;
+  }
 
-    setShowSizeHandle(val) {
-        this.showSizeHandle = val;
-    }
+  setShowSizeHandle(val) {
+    this.showSizeHandle = val;
+  }
 
-    getBrushSize() {
-        return this.brushSize[this.targetId];
-    }
-    getBrushAlpha() {
-        return this.brushAlpha[this.targetId];
-    }
-    moved = true;
+  getBrushSize() {
+    return this.brushSize[this.targetId];
+  }
+  getBrushAlpha() {
+    return this.brushAlpha[this.targetId];
+  }
+  moved = true;
 
-    setMoved(value) {
-        this.moved = value;
-    }
-    
+  setMoved(value) {
+    this.moved = value;
+  }
 }
 
 export const paintState = new PaintState();
